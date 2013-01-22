e38979ecb6ce7aefdb1dfc2ab3d6f2d0462592ee
Alejandro Diaz <user@server>
2013-01-21 22:51:16 -0800

    Tired for today. Ready first form for user input

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SuppliersApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
   match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/sqr', to: 'static_pages#sqr'
   match '/contact', to: 'static_pages#contact'
+  
+  
 
 
   # The priority is based upon order of creation:
